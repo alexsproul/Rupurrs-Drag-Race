ca8817660e1e15e569415026941c918b4c7251f4
Alex Sproul <alexsproul@hotmail.com>
2020-04-05 16:06:46 -0300

    Commented Character.java. Added modificatiosn to TODO.txt

--- Character.java
+++ Character.java
@@ -1,15 +1,32 @@
+/*
+    Rupurr's Cat Race: Character Class
+    A Character is the parent class for any character in the Rupurr's Cat Race game.
+    Every Character has a name and (eventually) will have a photo associated them.
+    Several subclasses extend Character.
+    Alexander Sproul - April 5, 2020
+*/
 public class Character {
-    private String name;
+    private String name; //The Character's name.
 
+    //CONSTRUCTOR:
+    //Single-argument Constructor: Only takes the name as a String for an argument.
     public Character(String name)
     {
         this.name = name;
     }
 
-    public void setName(String name) {this.name = name;}
+    //SETTER METHODS:
+    public void setName(String name) {this.name = name;} //Sets the Character's name attribute.
 
-    public String getName(){return this.name;}
+    //GETTER METHODS:
+    public String getName(){return this.name;} //Returns the Character's name as a String.
 
+    /*
+        TO STRING METHOD: 
+        Returns the Character's name as a String. All subclasses of Character
+        will inherit this toString() method and I currently can't anticipate
+        needing to override it.
+    */
     public String toString() {return this.name;}
 
 }
